cb0383ffa8ac813006bd30fbb2dbecf1c35f83e2
Barrydc146 <davidclint146@gmail.com>
2026-01-30 12:03:06 +0100

    Delete Rough Work directory

--- Rough Work/BooleanAccessControl/BooleanAccessControl01.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-// Phase 1: Understanding Boolean Basics
-// Start with the simplest boolean variable:
-pragma solidity ^0.8.0;
-
-contract AccessControl {
-    bool public isOpen;
-}
